0003d370816d7505917e539978e823e7471db1a3
Atchyut Nagabhairava <atchyutnagabhairava@gmail.com>
2019-01-16 11:41:42 +0530

    title: Added variables file
    desc: Examples for variable, constant declaration & concatination

---
+++ 01-basics/variables.js
@@ -0,0 +1,11 @@
+let game = 'snipper';
+
+let score = 100;
+
+const bonus = 20;
+
+let totalScore = score + bonus;
+
+message = "Total Score for " + game + ": " + totalScore;
+
+console.log(message);
